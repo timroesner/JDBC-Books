1ff1a6a0eeb3074eb7f0b9945448ad2513b99de4
timroesner <timroesner@me.com>
2017-11-02 19:41:36 -0700

    The usual setup

---
+++ src/Books.java
@@ -0,0 +1,15 @@
+/**
+ * JDBC-Books
+ * Authors: Tim Roesner,
+ * CS 157A
+ */
+
+import java.util.*;
+
+public class Books {
+
+    public static void main(String[]args) {
+        // Put executable code here
+    }
+    
+}
