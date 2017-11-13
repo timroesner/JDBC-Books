194c67c7e08ea55131724788eaaaaae1325f06d0
Ravin Sardal <ravin.sardal@gmail.com>
2017-11-12 16:53:14 -0800

    Insert test data into authorISBN table

--- src/InsertTestData.java
+++ src/InsertTestData.java
@@ -10,10 +10,11 @@ import java.util.*;
 public class InsertTestData {
 
 	public static void main(String[] argv) {
-		updateAuthorsTables();
+		// updateAuthorsTable();
+    updateAuthorISBNTable();
 	}
 
-	private static void updateAuthorsTables() {
+	private static void updateAuthorsTable() {
 		String authorFirstNames[] = { "Jane", "Dan", "Ralph Waldo", "F.Scott", "Ernest", "Walter", "Stephen", "Stieg",
 				"George", "Joanne K.", "John", "John R. R.", "Kurt", "Andy" };
 		String authorLastNames[] = { "Austen", "Brown", "Emerson", "Firtzgerald", "Grisham", "Hemingway", "Isaacson",
@@ -24,16 +25,36 @@ public class InsertTestData {
 			stmt = JDBC.connection.createStatement();
 
 			for (int i=1;i<=authorFirstNames.length;i++) {
-        String updateAuthorsTables = "INSERT INTO Authors (authorID, firstName, lastName)"
+        String updateAuthorsTable = "INSERT INTO Authors (authorID, firstName, lastName)"
   					+ "VALUES ("+i+", '"+ authorFirstNames[i-1] +"', '"+authorLastNames[i-1]+"')";
-  			stmt.executeUpdate(updateAuthorsTables);
+  			stmt.executeUpdate(updateAuthorsTable);
 			}
 		} catch (SQLException e) {
 			System.out.println("Connection Failed!");
 			e.printStackTrace();
 			return;
 		}
-
 		JDBC.close();
 	}
+
+  private static void updateAuthorISBNTable(){
+    String authorISBN[] = { "978-0141439518", "978-0307474278", "978-0142437629", "978-0743273565", "978-0345543240", "978-0684801223", "978-1501127625",
+				"978-1501175466", "978-0307949486", "978-0451524935", "978-0439708180", "978-0142000687", "978-0547928227", "978-0385333849", "978-0553418026" };
+		Statement stmt = null;
+		try {
+			JDBC.connect();
+			stmt = JDBC.connection.createStatement();
+
+			for (int i=1;i<=authorISBN.length;i++) {
+        String updateAuthorISBNTable = "INSERT INTO authorISBN (authorID, isbn)"
+  					+ "VALUES ("+i+", '"+ authorISBN[i-1] +"')";
+  			stmt.executeUpdate(updateAuthorISBNTable);
+			}
+		} catch (SQLException e) {
+			System.out.println("Connection Failed!");
+			e.printStackTrace();
+			return;
+		}
+		JDBC.close();
+  }
 }
