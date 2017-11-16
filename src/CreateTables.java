c0ec6fd34b03e59d9f5e67f915f5b12fab7dc804
Tim Roesner <timroesner@me.com>
2017-11-15 16:24:27 -0800

    Added delete everything function

--- src/CreateTables.java
+++ src/CreateTables.java
@@ -13,48 +13,66 @@ public class CreateTables {
 	   Statement stmt = null;
 	   try{
 
-	      // Open a connection
-	      JDBC.connect();
-	      
-	      // Execute a query
-	      stmt = JDBC.connection.createStatement();
-	      
-	      String authorsTable = "CREATE TABLE Authors " +
-	                   "(authorID INTEGER NOT NULL AUTO_INCREMENT, " +
-	                   " firstName CHAR(20), " + 
-	                   " lastName CHAR(20), " +  
-	                   " PRIMARY KEY (authorID))"; 
+	   	System.out.println("This will DELETE all data, do you want to continue? (y/n) ");
+		Scanner scanner = new Scanner(System.in);
+		String input = scanner.nextLine();
 
-	      stmt.executeUpdate(authorsTable);
-	      System.out.println("Created Authors table");
+		if(input == "y" || input == "Y") {
+			// Open a connection
+		    JDBC.connect();
 
-	      String titlesTable = "CREATE TABLE Titles " +
-	                   "(isbn CHAR(13) not NULL, " +
-	                   " title VARCHAR(255), " + 
-	                   " editionNumber INTEGER, " + 
-	                   " year CHAR(4), " + 
-	                   " publisherID INTEGER REFERENCES Publishers(publisherID), " + 
-	                   " price DECIMAL(8,2), " +
-	                   " PRIMARY KEY (isbn))"; 
+		    // Drop Tables
+		    String drop1 = "IF EXISTS(select * from sysobjects where name='Authors') DROP TABLE Authors ";
+		  	stmt.executeUpdate(drop1);
 
-	      stmt.executeUpdate(titlesTable);
-	      System.out.println("Created Titles table");
+		  	String drop2 = "IF EXISTS(select * from sysobjects where name='Titles') DROP TABLE Titles ";
+		  	stmt.executeUpdate(drop2);
 
-	      String publishersTable = "CREATE TABLE Publishers " +
-	                   "(publisherID INTEGER NOT NULL AUTO_INCREMENT, " +
-	                   " publisherName CHAR(100), " + 
-	                   " PRIMARY KEY (publisherID))"; 
+		  	String drop3 = "IF EXISTS(select * from sysobjects where name='Publishers') DROP TABLE Publishers ";
+		  	stmt.executeUpdate(drop3);
 
-	      stmt.executeUpdate(publishersTable);
-	      System.out.println("Created Publishers table");
+		  	String drop4 = "IF EXISTS(select * from sysobjects where name='authorISBN') DROP TABLE authorISBN ";
+		  	stmt.executeUpdate(drop4);
+		      
+		    // Create Tables
+		    stmt = JDBC.connection.createStatement();
+		      
+		    String authorsTable = "CREATE TABLE Authors " +
+		                   "(authorID INTEGER NOT NULL AUTO_INCREMENT, " +
+		                   " firstName CHAR(20), " + 
+		                   " lastName CHAR(20), " +  
+		                   " PRIMARY KEY (authorID))"; 
 
-	      String authorISBNTable = "CREATE TABLE authorISBN " +
-	                   "(authorID INTEGER REFERENCES Authors(authorID), " +
-	                   " isbn CHAR(10) REFERENCES Titles(isbn))";  
+		    stmt.executeUpdate(authorsTable);
+		    System.out.println("Created Authors table");
 
-	      stmt.executeUpdate(authorISBNTable);
-	      System.out.println("Created authorISBN table");
+		    String titlesTable = "CREATE TABLE Titles " +
+		                   "(isbn CHAR(13) not NULL, " +
+		                   " title VARCHAR(255), " + 
+		                   " editionNumber INTEGER, " + 
+		                   " year CHAR(4), " + 
+		                   " publisherID INTEGER REFERENCES Publishers(publisherID), " + 
+		                   " price DECIMAL(8,2), " +
+		                   " PRIMARY KEY (isbn))"; 
 
+		    stmt.executeUpdate(titlesTable);
+		    System.out.println("Created Titles table");
+
+		    String publishersTable = "CREATE TABLE Publishers " +
+		                   "(publisherID INTEGER NOT NULL AUTO_INCREMENT, " +
+		                   " publisherName CHAR(100), " + 
+		                   " PRIMARY KEY (publisherID))"; 
+
+		    stmt.executeUpdate(publishersTable);
+		    System.out.println("Created Publishers table");
+
+		    String authorISBNTable = "CREATE TABLE authorISBN " +
+		                   "(authorID INTEGER REFERENCES Authors(authorID), " +
+		                   " isbn CHAR(10) REFERENCES Titles(isbn))";  
+
+		    stmt.executeUpdate(authorISBNTable);
+		    System.out.println("Created authorISBN table");
+		}
 	   } catch(SQLException se) {
 	      // Handle errors for JDBC
 	      se.printStackTrace();
