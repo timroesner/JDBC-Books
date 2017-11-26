1734ad3f616795a5883d77cc61defbfd947a2942
timroesner <timroesner@me.com>
2017-11-26 13:33:31 -0800

    Added print out statement

--- src/Query6.java
+++ src/Query6.java
@@ -23,14 +23,14 @@ public class Query6 {
 	      String query6Publisher = "INSERT INTO Publishers (publisherName)" +
 	      				       "VALUES ('Crown')"; 
 	      stmt.executeUpdate(query6Publisher);
-	      System.out.println("Added Publisher");
+	      System.out.println("Added Publisher 'Crown' ");
 
 	      
 	      // Add new title
 	      String query6Title = "INSERT INTO Titles (isbn, title, editionNumber, year, publisherID, price)" +
 	      				       "VALUES ('0553448122', 'Artemis: A Novel', '1', '2017', '12', '16.20')"; 
 	      stmt.executeUpdate(query6Title);
-	      System.out.println("Added title");
+	      System.out.println("Added title 'Artemis: A Novel' ");
 
 
 	      // Link new title to author (In this case Andy Weir)
@@ -38,6 +38,38 @@ public class Query6 {
 	      				       "VALUES ('15', '0553448122')"; 
 	      stmt.executeUpdate(query6authorISBN);
 	      System.out.println("Added authorISBN relation");
+
+	      // Print modified tables
+	      String exampleQuery2 = "SELECT * FROM Titles"; 
+	      System.out.println("Titles:");
+	      ResultSet rs2 = stmt.executeQuery(exampleQuery2);
+          while (rs2.next()) {
+            String isbn = rs2.getString("isbn");
+            String title = rs2.getString("title");
+            int edition = rs2.getInt("editionNumber");
+            String year = rs2.getString("year");
+            int pubID = rs2.getInt("publisherID");
+            float price = rs2.getFloat("price");
+            System.out.println(isbn + "\t" + title + "\t" + edition + "\t" + year + "\t" + pubID + "\t" + price);
+          }
+
+	      String exampleQuery3 = "SELECT * FROM Publishers"; 
+	      System.out.println("Publishers:");
+	      ResultSet rs3 = stmt.executeQuery(exampleQuery3);
+          while (rs3.next()) {
+          	int id = rs3.getInt("publisherID");
+            String name = rs3.getString("publisherName");
+            System.out.println(id + "\t" + name);
+          }
+
+	      String exampleQuery4 = "SELECT * FROM authorISBN";
+	      System.out.println("authorISBN:"); 
+	      ResultSet rs4 = stmt.executeQuery(exampleQuery4);
+          while (rs4.next()) {
+          	int id = rs4.getInt("authorID");
+          	String isbn = rs4.getString("isbn");
+            System.out.println(id + "\t" + isbn);
+          }
 
 	   } catch(SQLException se) {
 	      //Handle errors for JDBC
