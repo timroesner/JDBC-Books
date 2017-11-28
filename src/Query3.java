aaabfafbf639bab8d06abcd4a1c8ef09bd670172
Shweta More <ssmore6@gmail.com>
2017-11-27 20:29:25 -0800

    modified console results

--- src/Query3.java
+++ src/Query3.java
@@ -23,22 +23,30 @@ public class Query3 {
 	      // Execute a query
 	      stmt = JDBC.connection.createStatement();
           
-          String query3 = "SELECT isbn, title, year, publisherID FROM titles WHERE publisherID=3 ORDER BY title";
+          String query3_name = "SELECT publisherID, publisherName FROM publishers WHERE publisherID=3";
+          
+          ResultSet rs1 = stmt.executeQuery(query3_name);
+          String name = null;
+          while(rs1.next()){
+              int id = rs1.getInt("publisherID");
+              name = rs1.getString("publisherName");
+          }
+          
+          System.out.println();
+          System.out.println("--------------------------------------------------");
+          System.out.println("List of books by the publisher " + name + ":");
+          System.out.println("--------------------------------------------------");
+              
+          String query3_books = "SELECT isbn, title, year, publisherID FROM titles WHERE publisherID=3 ORDER BY title";
+          
+	      ResultSet rs2 = stmt.executeQuery(query3_books);
            
-	      ResultSet rs = stmt.executeQuery(query3);
-	      System.out.println();
-          System.out.println("--------------------------------------------------");
-          System.out.println("List of books by selected publisher");
-          System.out.println("--------------------------------------------------");
-
-          while(rs.next()){
-                  int id = rs.getInt("publisherID");
-                  String isbn = rs.getString("isbn");
-                  String title = rs.getString("title");
-                  String year = rs.getString("year");
+          while(rs2.next()){
+                  int id = rs2.getInt("publisherID");
+                  String isbn = rs2.getString("isbn");
+                  String title = rs2.getString("title");
+                  String year = rs2.getString("year");
                   System.out.println(id + " - " + title + " - " + year + " - " + isbn);
-              
-              
           }
           System.out.println("--------------------------------------------------");
           System.out.println();
